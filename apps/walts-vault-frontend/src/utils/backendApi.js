15069fda5f892ca49d49b35f2ac30f779a35ebfe
Harshil-Vaghasiya <harshilvaghasiya25@gmail.com>
2023-04-29 11:37:28 +0530

    change:solve baseurl issue

--- apps/walts-vault-frontend/src/utils/backendApi.js
+++ apps/walts-vault-frontend/src/utils/backendApi.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "https://seahorse-app-dpo4n.ondigitalocean.app";
+const BASE_URL = "https://seahorse-app-dpo4n.ondigitalocean.app/signer";
 
 export const getOrderSignature = async (address) => {
   try {
-    const { data } = await axios.post(BASE_URL + "/signer/order", { address: address });
+    const { data } = await axios.post(BASE_URL + "/order", { address: address });
     return data;
   } catch (err) {
     return false;
